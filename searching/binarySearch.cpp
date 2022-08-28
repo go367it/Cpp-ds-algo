d5e24592f05abe5188283cb6b897a2b1fa04feb3
go367it <satyamkumar3001@gmail.com>
2022-08-28 09:52:30 +0530

    binary search

---
+++ searching/binarySearch.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int x){
+    int low = 0, high = n-1;
+    while(low <= high){
+        int mid = (low + high)/2;
+        if(arr[mid] == x){
+            return mid;
+        }
+        else if(arr[mid] > x){
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int arr[] = {10, 20, 30, 40, 50, 60}, n = 6;
+	int x = 50;
+    int res = binarySearch(arr, n ,x);
+    cout<<res;
+    return 0;
+}
